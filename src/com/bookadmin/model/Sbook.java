a8b97a99864d44749f862e7b9104041695df854b
wanglinhong_cd <wanglinhong_cd@keruyun.com>
2018-01-25 17:33:05 +0800

    网点图书

--- src/com/bookadmin/model/Sbook.java
+++ src/com/bookadmin/model/Sbook.java
@@ -8,8 +8,43 @@ public class Sbook {
 	private String storename;
 	private String booknumber;
 	private int amount;
+	private String average;
+	private String author;
+	private String tags0;
+	private String tags1;
+	private String tags2;
+	private int bookState;		//书籍状态  0：未入库    1：在库   2：借出
 	
-	
+	public String getAverage() {
+		return average;
+	}
+	public void setAverage(String average) {
+		this.average = average;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getTags0() {
+		return tags0;
+	}
+	public void setTags0(String tags0) {
+		this.tags0 = tags0;
+	}
+	public String getTags1() {
+		return tags1;
+	}
+	public void setTags1(String tags1) {
+		this.tags1 = tags1;
+	}
+	public String getTags2() {
+		return tags2;
+	}
+	public void setTags2(String tags2) {
+		this.tags2 = tags2;
+	}
 	public Sbook(){}
 	public int getBookid() {
 		return bookid;
@@ -46,6 +81,12 @@ public class Sbook {
 	}
 	public void setAmount(int amount) {
 		this.amount = amount;
+	}
+	public int getBookState() {
+		return bookState;
+	}
+	public void setBookState(int bookState) {
+		this.bookState = bookState;
 	}
 	
 	
